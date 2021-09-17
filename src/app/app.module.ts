15c195110072d3714455928078dad883899a3923
knazark <knazark@tenantcloud.com>
2021-09-17 11:36:24 +0300

    misc: Added package ngxs & fake-backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { fakeBackendProvider } from './helpers/fake-backend';
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule],
-	providers: [],
+	providers: [
+		// provider used to create fake backend
+		fakeBackendProvider,
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
